3234b271788ded26b6d0d53e3ecb74dfcb1090bb
Alexander Rovang <wolf_vii@yahoo.com>
2020-12-08 14:24:14 -0500

    commit tree 2d0ddfacea67195bb2b85e72b42de106ad35a549

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  save = array.length - 1
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    last = array[save]
+    array.pop
+    first = array.join(", ")
+    first + ", and " + last
+  else
+    array.join
+  end 
 end
